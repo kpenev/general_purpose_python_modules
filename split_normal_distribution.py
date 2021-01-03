aa26a4fa99011043012c0c9931354f848906858c
Kaloyan Penev <kpenev@gmail.com>
2021-01-03 17:00:45 -0600

    Add ability to define split-normals from asymmetric error bars

--- split_normal_distribution.py
+++ split_normal_distribution.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot
 from scipy.stats import rv_continuous, norm
 import numpy
 from scipy.integrate import solve_ivp
+from scipy.special import erf
+from scipy.optimize import fsolve
 
 class SplitNormal(rv_continuous):
     r"""
@@ -43,15 +45,13 @@ class SplitNormal(rv_continuous):
 
         return numpy.where(
             x < 0,
-            2.0 / (stddev_ratio + 1) * norm.cdf(norm_arg),
+            2.0  * norm.cdf(norm_arg),
             (
                 1.0
                 +
                 stddev_ratio * (2.0 * norm.cdf(norm_arg) - 1.0)
-            ) / (
-                stddev_ratio + 1
             )
-        )
+        ) / (stddev_ratio + 1)
 
     def _logcdf(self, x, stddev_ratio):
 
@@ -102,13 +102,57 @@ class SplitNormal(rv_continuous):
     def _isf(self, q, stddev_ratio):
 
         return -self._ppf(q, 1.0 / stddev_ratio)
+
+    def freeze_error_bar(self,
+                         mode=0.0,
+                         abs_plus_error=1.0,
+                         abs_minus_error=1.0,
+                         confidence=erf(2.0**-0.5)):
+        """Freeze the distribution given asymmetric error bar parameters."""
+
+        right_quantile = (1.0 + confidence) / 2.0
+        left_quantile = (1.0 - confidence) / 2.0
+
+        def equations(args):
+            """Defines the sysetem of equations that must be solved."""
+
+            return [
+                self.cdf(
+                    abs_plus_error,
+                    stddev_ratio=args[0],
+                    scale=args[1]
+                ) - right_quantile,
+                self.cdf(
+                    -abs_minus_error,
+                    stddev_ratio=args[0],
+                    scale=args[1]
+                ) - left_quantile
+            ]
+
+        stddev_ratio, scale = fsolve(
+            equations,
+            [
+                abs_plus_error / abs_minus_error,
+                0.5 * (abs_plus_error + abs_minus_error)
+            ]
+        )
+
+        return self(
+            loc=mode,
+            stddev_ratio=stddev_ratio,
+            scale=scale
+        )
+
     #pylint: disable=arguments-differ
 
 split_normal = SplitNormal(name='split-normal')
 
 if __name__ == '__main__':
-    plot_distro = split_normal(stddev_ratio=2.0, scale=3.0)
-    plot_x = numpy.linspace(-10.0, 10.0, 1000)
+    error_bar = dict(mode=5.0,
+                     abs_plus_error=1.2,
+                     abs_minus_error=0.6)
+    plot_distro = split_normal.freeze_error_bar(**error_bar)
+    plot_x = numpy.linspace(0.0, 10.0, 1000)
 
     pyplot.plot(plot_x,
                 plot_distro.pdf(plot_x),
@@ -126,7 +170,8 @@ if __name__ == '__main__':
     pdf_integral = solve_ivp(lambda t, y: plot_distro.pdf(t),
                              (-20.0, 20.0),
                              y0=numpy.array([0.0]),
-                             t_eval=plot_x).y.flatten()
+                             t_eval=plot_x,
+                             max_step=0.1).y.flatten()
     pyplot.plot(plot_x,
                 pdf_integral,
                 ':g',
@@ -139,6 +184,11 @@ if __name__ == '__main__':
                 1.0 - plot_distro.sf(plot_x[::10]),
                 '+c',
                 linewidth=3)
+    pyplot.axhline((1.0 - erf(2.0**-0.5)) / 2.0)
+    pyplot.axhline((1.0 + erf(2.0**-0.5)) / 2.0)
+    pyplot.axvline(error_bar['mode'] - error_bar['abs_minus_error'])
+    pyplot.axvline(error_bar['mode'] + error_bar['abs_plus_error'])
+
 
     pyplot.title('CDF')
     pyplot.show()
@@ -155,6 +205,10 @@ if __name__ == '__main__':
                 plot_distro.isf(1.0 - plot_q[::10]),
                 'xb',
                 linewidth=3)
+    pyplot.axvline((1.0 - erf(2.0**-0.5)) / 2.0)
+    pyplot.axvline((1.0 + erf(2.0**-0.5)) / 2.0)
+    pyplot.axhline(error_bar['mode'] - error_bar['abs_minus_error'])
+    pyplot.axhline(error_bar['mode'] + error_bar['abs_plus_error'])
 
-    pyplot.plot('PPF')
+    pyplot.title('PPF')
     pyplot.show()

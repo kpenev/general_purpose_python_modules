e1d3b992fe9e315ef7c59aeba8180e0b6b21beaf
Kaloyan Penev <kpenev@gmail.com>
2021-04-01 16:57:15 -0500

    Start collecting general purpose visualizations

---
+++ visuals.py
@@ -0,0 +1,19 @@
+import numpy
+from matplotlib import pyplot
+from corner import corner
+
+def make_corner_plot(plot_data_frame, corner_plot_fname=''):
+    """Create a corner plot of the given pandas DataFrame and save/display."""
+
+    plot_ranges = [
+        (
+            numpy.min(values[numpy.isfinite(values)]),
+            numpy.max(values[numpy.isfinite(values)])
+        )
+        for _, values in plot_data_frame.items()
+    ]
+    corner(plot_data_frame, range=plot_ranges)
+    if corner_plot_fname:
+        pyplot.savefig(corner_plot_fname)
+    else:
+        pyplot.show()

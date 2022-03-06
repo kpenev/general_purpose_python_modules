b35e79ff3b57aad02b16542cd0e8a95ed101b008
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2022-03-06 11:48:21 -0600

    Better arganization of combined constraint

--- combined_mcmc_constraint.py
+++ combined_mcmc_constraint.py
@@ -1,6 +1,7 @@
 """Define class for combining unrelated MCMC constraints of a parameter."""
 
 import numpy
+from KDEpy import NaiveKDE as KDE
 
 class CombinedMCMCConstraint:
     """
@@ -15,6 +16,11 @@ class CombinedMCMCConstraint:
         self._pdf = 1.0
         self._kernel_scale = kernel_scale
 
+    def eval_kernel(self, shifted_scaled):
+        """The kernel to apply shifted to zero and scaled to unit width."""
+
+        return numpy.maximum(0.75 * (1.0 - shifted_scaled**2), 0.0)
+
     def add_samples(self, samples):
         """
         Update the PDF with the given samples.
@@ -24,16 +30,18 @@ class CombinedMCMCConstraint:
         """
 
         nsamples = samples.shape[-1]
-        kernel_width = self._kernel_scale * nsamples**(-1.0 / 3.0)
+        kernel_width = max(self._kernel_scale * nsamples**(-1.0 / 3.0),
+                           (self._grid[1:] - self._grid[:-1]).min())
+        kernel_width = 0.3
         self._pdf *= 1.0 / kernel_width * numpy.mean(
-            numpy.exp(
+            self.eval_kernel(
                 -(
                     (
                         samples.flatten() - self._grid[:, None]
                     ).reshape(
                         self._grid.shape + samples.shape
                     )
-                )**2
+                )
                 /
                 kernel_width
             ),

3fc60654493127538792a24163b6f2d1d6ae92ce
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2022-04-11 17:31:18 -0500

    More flexible corner plots

--- visuals.py
+++ visuals.py
@@ -2,7 +2,7 @@ import numpy
 from matplotlib import pyplot
 from corner import corner
 
-def make_corner_plot(plot_data_frame, corner_plot_fname=''):
+def make_corner_plot(plot_data_frame, corner_plot_fname='', **corner_kwargs):
     """Create a corner plot of the given pandas DataFrame and save/display."""
 
     plot_ranges = [
@@ -12,7 +12,9 @@ def make_corner_plot(plot_data_frame, corner_plot_fname=''):
         )
         for _, values in plot_data_frame.items()
     ]
-    corner(plot_data_frame, range=plot_ranges)
+    corner(plot_data_frame,
+           range=plot_ranges,
+           **corner_kwargs)
     if corner_plot_fname:
         pyplot.savefig(corner_plot_fname)
     else:

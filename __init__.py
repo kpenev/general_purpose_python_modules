c08d1656d301c12fe65d592e5e4752b2296f8c9d
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-02-21 14:29:01 -0600

    Avoid error in __init__ if POET is not present

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 from .multi_pickle import MultiPickle
 from .kde import KDEDistribution
-from .reproduce_system import find_evolution
 from .split_normal_distribution import split_normal
+
+try:
+    from .reproduce_system import find_evolution
+except:
+    pass

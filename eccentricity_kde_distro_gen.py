3cf2b08db95aa3a9b7335f38e64dc47670e40325
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-05-18 19:22:12 -0500

    Begin KDE eccentricity distribution with different sin/cos kernels

--- eccentricity_kde_distro_gen.py
+++ eccentricity_kde_distro_gen.py
@@ -11,8 +11,8 @@ import numpy
 #Method arguments also come from scipy.stats
 #pylint: disable=invalid-name
 #pylint: disable=arguments-differ
-class eccentricity_kde_distro_gen(stats.rv_continuous):
-    r"""RV with PDF built by KDE of samples of eccentricity.
+class eccentricity_circular_kde_distro_gen(stats.rv_continuous):
+    r"""RV with PDF given by KDE of (esinw,ecosw) samples for circular kernel.
 
     %(before_notes)s
 
@@ -62,11 +62,12 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
                                 s_to_n + 10.0,
                                 10)
         points = numpy.concatenate(([0], points[points>0]))
-        return (
+        result = (
             quad(
                 self._norm_integrand,
                 a=0,
-                b=points[-1], points=points,
+                b=points[-1],
+                points=points,
                 args=(s_to_n,)
             )[0]
             +
@@ -77,6 +78,10 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
                 args=(s_to_n,)
             )[0]
         )
+        if self._uniform_e_samples:
+            return result * center
+        else:
+            return result
 
 
     def _pdf(self, x):
@@ -93,7 +98,12 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
                 *
                 special.i0e(eval_x*self._s_to_n)
             )
-        ).sum(axis=0) * self._norm
+        )
+
+        if self._uniform_e_samples:
+            result *= self._s_to_n * self._width
+
+        result = result.sum(axis=0) * self._norm
 
         try:
             return result.reshape(x.shape)
@@ -129,7 +139,11 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
         )
 
 
-    def __init__(self, e_samples, kernel_width, **kwargs):
+    def __init__(self,
+                 e_samples,
+                 kernel_width,
+                 uniform_e_samples=False,
+                 **kwargs):
         r"""
         Specify the samples, kernel width, and distribution config.
 
@@ -139,6 +153,9 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
 
             kernel_width(float):    The with of the kernel in
                 :math:`e\sin\omega` and `e\cos\omega` space (assumed the same).
+
+            uniform_e_samples(bool):    Were the samples generated assuming a
+                uniform prior on e?
 
             **kwargs:    Any arguments other than ``a``, ``b``, and ``name`` to
                 pass directly to `scipy.stats.rv_continuous.__init__`.
@@ -151,12 +168,118 @@ class eccentricity_kde_distro_gen(stats.rv_continuous):
         e_samples = numpy.atleast_1d(e_samples)[:, None]
         self._width = kernel_width
         self._s_to_n = e_samples / kernel_width
+        self._uniform_e_samples = uniform_e_samples
         self._norm = 1.0 / numpy.vectorize(self._calc_norm)(
             e_samples,
             kernel_width
         ).sum() / kernel_width
 #pylint: enable=invalid-name
 #pylint: enable=arguments-differ
+
+class eccentricity_noncircular_kde_distro_gen(stats.rv_continuous):
+    r"""RV with PDF given by KDE of (esinw,ecosw) samples for non-circular kern.
+
+    %(before_notes)s
+
+    Notes
+    -----
+
+    Each eccentricity component is allowed to have a different kernel. The
+    probability density function is built by numerical integration over the
+    periapsis angle.
+
+    .. math::
+
+        f(e) = \int_0^{2\pi} d\omega
+            \sum_i k_c[e'\cos\omega'-(e\cos\omega)_i]
+                   k_s[e'\sin\omega'-(e\sin\omega)_i] e_i^n
+
+    Where :math:`k_s` and :math:`k_c` are the kernels to use for
+    :math:`e\sin\omega` and :math:`e\cos\omega` respectively,
+    :math:`(e\sin\omega)_i` and :math:`(e\cos\omega)_i` are the samples we are
+    building the distribution from, and :math:`n` is zero if samples were built
+    with independent uniform priors on :math:`e\sin\omega` and
+    :math:`e\cos\omega` were used for sampling and 1 if uniform prior on
+    :math:`e` was used. Note that the area element in the integration is taken
+    as :math:`de\,d\omega` instead of :math:`e de d\omega` to impose uniform
+    distribution on :math:`e` around a given sample.
+
+    %(after_notes)s
+
+    %(example)s
+    """
+
+    def __init__(self,
+                 esinw_samples,
+                 ecosw_samples,
+                 sin_kernel,
+                 cos_kernel,
+                 uniform_e_samples=False,
+                 **kwargs):
+
+        r"""
+        Specify the samples, the kernels, and distribution config.
+
+        The kernels should be scipy stats like distributions, assumed to apply
+        to all samples of a given eccentricity component and should already have
+        the correct bandwidth set. If picklable object is required, specify the
+        kernel as 3-tuple:
+                    * str:    The name of the distribution in `scipy.stats`
+                    * tuple:    Positional arguments for kernel ``__init__``
+                    * dict:    Keyword arguments for kernel ``__init__``
+
+        Args:
+            esinw_samples(array):    The samples of :math:`e\sin\omega`  to
+                build the distribution from.
+
+            ecosw_samples(array):    The samples of :math:`e\cos\omega`  to
+                build the distribution from.
+
+            sin_kernel(rv_continuous):    The kernel to convolve
+                :math:`e\sin\omega` samples with.
+
+            cos_kernel(rv_continuous):    The kernel to convolve
+                :math:`e\cos\omega` samples with.
+
+            uniform_e_samples(bool):    Were the samples generated assuming a
+                uniform prior on e?
+
+            **kwargs:    Any arguments other than ``a``, ``b``, and ``name`` to
+                pass directly to `scipy.stats.rv_continuous.__init__`.
+
+        Returns:
+            None
+        """
+
+        super().__init__(a=0.0, name="EccentricityDistro", **kwargs)
+
+        self._kernel_config = dict()
+        self._kernel = dict()
+        self._samples = dict()
+
+        for component in ['sin', 'cos']:
+            kernel = locals()[component + '_kernel']
+            if isinstance(kernel, tuple):
+                self._kernel_config[component] = kernel
+                self._kernel = getattr(stats, kernel[0])(*kernel[1], **kernel[2])
+            else:
+                self._kernel_config[component] = None
+                self._kernel[component] = kernel
+
+            self._samples[component] = numpy.ravel(
+                locals()['e' + component + 'wsamples']
+            )
+
+        <++>
+
+        self._weights = weights
+        kernel_min, kernel_max = self._kernel.support()
+        self._support = ()
+        super().__init__(a=(self._samples.min() + kernel_min),
+                         b=(self._samples.max() + kernel_max))
+
+
+
 
 if __name__ == '__main__':
     val=0.05

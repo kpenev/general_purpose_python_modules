f5f24fb8941352f1683f55d841b3815fe0b967af
Joshua Aaron Schussler <vortebo@kartof>
2023-05-22 11:15:04 -0500

    Made setup_process() a little more general, and made reproduce_system() use logging more reliably. Fixed issues with how test_find_evolution functions handled values returned from find_evolution(). Fixed multiple issues revealed by testing. General cleanup.

--- multiprocessing_util.py
+++ multiprocessing_util.py
@@ -24,8 +24,39 @@ def get_code_version_str():
         return head_sha + ':dirty'
     return head_sha
 
-def setup_process(config, task='calculate'):
-    """Logging and I/O setup for the current processes."""
+def setup_process(fname_datetime_format = '%Y%m%d%H%M%S',
+                  system = "test_system",
+                  std_out_err_fname = 'sampling_output/%(system)s_%(now)s_%(pid)d.outerr',
+                  logging_fname = 'sampling_output/%(system)s_%(now)s_%(pid)d.log',
+                  logging_verbosity = 'debug',
+                  logging_message_format = ('%(levelname)s %(asctime)s %(name)s: %(message)s | '
+                          '%(pathname)s.%(funcName)s:%(lineno)d'), # default_logging_format (bayesian.basic_util)
+                  logging_datetime_format = None,
+                  task='calculate'):
+    """
+    Logging and I/O setup for the current processes.
+
+    Args:
+        fname_datetime_format(str):    The format string for the current time
+
+        system(str):    The name of the system being sampled.
+
+        std_out_err_fname(str):    The format string for the name of the file
+            to which stdout and stderr will be redirected. 
+
+        logging_fname(str):    The format string for the name of the file to
+            which logging messages will be written.
+
+        logging_verbosity(str):    The verbosity level for logging.
+            Options: 'debug', 'info', 'warning', 'error', 'critical'
+        
+        logging_message_format(str):    The format string for logging messages.
+
+        logging_datetime_format(str):    The format string for the datetime
+            component of logging messages.
+        
+        task(str):    The task being performed by the current process.
+    """
 
     def ensure_directory(fname):
         """Make sure the directory containing the given name exists."""
@@ -35,13 +66,13 @@ def setup_process(config, task='calculate'):
             os.makedirs(dirname)
 
     fname_substitutions = dict(
-        now=datetime.now().strftime(config.fname_datetime_format),
-        system=config.system,
+        now=datetime.now().strftime(fname_datetime_format),
+        system=system,
         pid=os.getpid(),
         task=task
     )
 
-    std_out_err_fname = config.std_out_err_fname % fname_substitutions
+    std_out_err_fname = std_out_err_fname % fname_substitutions
     ensure_directory(std_out_err_fname)
 
     io_destination = os.open(
@@ -52,7 +83,7 @@ def setup_process(config, task='calculate'):
     os.dup2(io_destination, 1)
     os.dup2(io_destination, 2)
 
-    logging_fname = config.logging_fname % fname_substitutions
+    logging_fname = logging_fname % fname_substitutions
     ensure_directory(logging_fname)
 
     for handler in logging.root.handlers[:]:
@@ -60,9 +91,9 @@ def setup_process(config, task='calculate'):
         handler.close()
     logging_config = dict(
         filename=logging_fname,
-        level=getattr(logging, config.logging_verbosity.upper()),
-        format=config.logging_message_format,
+        level=getattr(logging, logging_verbosity.upper()),
+        format=logging_message_format,
     )
-    if config.logging_datetime_format is not None:
-        logging_config['datefmt'] = config.logging_datetime_format
+    if logging_datetime_format is not None:
+        logging_config['datefmt'] = logging_datetime_format
     logging.basicConfig(**logging_config)

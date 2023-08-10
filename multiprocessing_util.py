18f5f35239d3c9ce3ebfd072f5dbc72f5f1532e9
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-08-10 13:15:49 -0500

    More convenient interface to setup_process

--- multiprocessing_util.py
+++ multiprocessing_util.py
@@ -59,7 +59,7 @@ def setup_process(**config):
         """Make sure the directory containing the given name exists."""
 
         dirname = os.path.dirname(fname)
-        if not os.path.exists(dirname):
+        if dirname and not os.path.exists(dirname):
             os.makedirs(dirname)
 
     if 'task' not in config:
@@ -69,18 +69,20 @@ def setup_process(**config):
         pid=os.getpid()
     )
 
-    std_out_err_fname = config['std_out_err_fname'].format(config)
-    ensure_directory(std_out_err_fname)
+    if config.get('std_out_err_fname') is not None:
+        print('Config: ' + repr(config))
+        std_out_err_fname = config['std_out_err_fname'].format_map(config)
+        ensure_directory(std_out_err_fname)
 
-    io_destination = os.open(
-        std_out_err_fname,
-        os.O_WRONLY | os.O_TRUNC | os.O_CREAT | os.O_DSYNC,
-        mode=0o666
-    )
-    os.dup2(io_destination, 1)
-    os.dup2(io_destination, 2)
+        io_destination = os.open(
+            std_out_err_fname,
+            os.O_WRONLY | os.O_TRUNC | os.O_CREAT | os.O_DSYNC,
+            mode=0o666
+        )
+        os.dup2(io_destination, 1)
+        os.dup2(io_destination, 2)
 
-    logging_fname = config['logging_fname'].format(config)
+    logging_fname = config['logging_fname'].format_map(config)
     ensure_directory(logging_fname)
 
     for handler in logging.root.handlers[:]:
@@ -88,9 +90,17 @@ def setup_process(**config):
         handler.close()
     logging_config = dict(
         filename=logging_fname,
-        level=getattr(logging, config['logging_verbosity'].upper()),
+        level=getattr(
+            logging,
+            config.get('logging_verbosity', config.get('verbose')).upper()
+        ),
         format=config['logging_message_format']
     )
-    if config['logging_datetime_format'] is not None:
+    if config.get('logging_datetime_format') is not None:
         logging_config['datefmt'] = config['logging_datetime_format']
     logging.basicConfig(**logging_config)
+
+def setup_process_map(config):
+    """Like `setup_process`, but more convenient for `multiprocessing.Pool`."""
+
+    setup_process(**config)

5518d84685249b6f7b3031ff0a81b123afe703e7
Ashkania <sxj190009@utdallas.edu>
2025-04-18 11:52:30 -0500

    use flag O_DSYNC only if the os supports it

--- multiprocessing_util.py
+++ multiprocessing_util.py
@@ -140,9 +140,14 @@ def setup_process(**config):
     if std_out_err_fname is not None:
         ensure_directory(std_out_err_fname)
 
+        # Define flags for opening the file
+        flags = os.O_WRONLY | os.O_TRUNC | os.O_CREAT
+        if hasattr(os, "O_DSYNC"):  # Check if os.O_DSYNC exists
+            flags |= os.O_DSYNC
+
         io_destination = os.open(
             std_out_err_fname,
-            os.O_WRONLY | os.O_TRUNC | os.O_CREAT | os.O_DSYNC,
+            flags,
             mode=0o666,
         )
         os.dup2(io_destination, 1)
